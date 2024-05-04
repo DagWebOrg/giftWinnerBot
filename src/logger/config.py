dd0744541b385bccfdf42469636e7d3db1c02818
Kamil Alimetov <62622296+Clementine1703@users.noreply.github.com>
2024-05-05 01:03:55 +0900

    Update config.py

--- src/logger/config.py
+++ src/logger/config.py
@@ -2,7 +2,7 @@ import logging
 
 
 config = {
-    "filename": "src/logger/logs/critical.log",
+    "filename": "logger/logs/critical.log",
     "level": logging.INFO,
     "format": "%(asctime)s - %(levelname)s - %(filename)s - %(lineno)s - %(message)s"
 }
