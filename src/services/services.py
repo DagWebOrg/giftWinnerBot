63ecc1626a173d99c21407c297585eeaec95a602
Камиль Алиметов <kamamber@MacBook-Air-Kamil.local>
2024-05-05 01:17:07 +0900

    refactoring (unstable)

--- src/services/services.py
+++ src/services/services.py
@@ -5,46 +5,18 @@ import time
 
 from vk_api import auth
 from database.crud import CRUD
-from .http_requests.api import get_posts_from_api
 from logger.config import LOGGER
+
+from .utils import get_posts_from_tracked_accounts, get_validated_posts
 
 
 async def add_all_new_posts_to_database():
-    tracking_accounts = CRUD.get_tracking_accounts()
-    validated_posts = []
+    posts = await get_posts_from_tracked_accounts()
+    validated_posts = get_validated_posts(posts)
 
-    # переделать под GATHER асинхронно РАЗДЕЛИТЬ НА ФУНКЦИИ КОД МАКСИМУМ 10 строк
-    for account in tracking_accounts:
-        print(account['last_scan_data'].timestamp(), account['alias'])
-
-        response_obj = await get_posts_from_api(account['account_id'], count=1)
-
-        # Если вернулся ответ с ошибкой.
-        if 'error' in response_obj.keys():
-            error_msg = response_obj.get('error').get('error_msg')
-            LOGGER.error(f'Ошибка при сканировании аккаунта {account["account_id"]}:\n{error_msg}')
-            continue
-
-        posts = response_obj['response'].get('items', [])
-
-        for post in posts:
-
-            if post_validate(account=account, post=post):
-                original_post = post['copy_history'][0]
-                post_id = f"wall{str(original_post['owner_id'])}_{str(original_post['id'])}"
-                content = original_post['text'][:100]
-
-                validated_posts.append({
-                'post_id': post_id,
-                'content': content,
-                })
-
-        if posts:
-            CRUD.delete_tracking_account(account['alias'])
-            CRUD.create_tracking_account(alias=account['alias'], account_id=account['account_id'])
-    
-    CRUD.create_posts(posts=validated_posts)
+    CRUD.create_posts(validated_posts)
     print(validated_posts)
+
 
 
 async def repost_all_new_posts_from_database():
